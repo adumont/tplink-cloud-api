5ff960bb899d52c660472c4594df595b28e267e4
Alexandre Dumont <adumont@gmail.com>
2017-07-25 20:39:11 +0200

    Add headers and license to all files

--- tplink.js
+++ tplink.js
@@ -1,3 +1,26 @@
+/**
+ * @package     tplink-hs100-cloud-api
+ * @author      Alexandre Dumont <adumont@gmail.com>
+ * @copyright   (C) 2017 - Alexandre Dumont
+ * @license     https://www.gnu.org/licenses/gpl-3.0.txt
+ * @link        http://itnerd.space
+ */
+
+/* This file is part of tplink-hs100-cloud-api.
+
+tplink-hs100-cloud-api is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+tplink-hs100-cloud-api is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+Foobar. If not, see http://www.gnu.org/licenses/. */
+
 var rp = require('request-promise');
 
 class TPLink {
