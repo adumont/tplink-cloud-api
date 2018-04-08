b258e5a723af4772fde02e2451ecb73cd55521b8
Alexandre Dumont <adumont@gmail.com>
2018-04-09 00:24:27 +0200

    modified test for HS100

--- test/testHS100.js
+++ test/testHS100.js
@@ -3,19 +3,33 @@
 const expect = require('chai').expect
 const tplink = require('../distribution/tplink')
 
-describe('HS110', () => {
+describe('HS100', () => {
 
   it('should be able to login', async () => {
     const myTPLink = await tplink.login(process.env.TEST_LOGIN_EMAIL, process.env.TEST_LOGIN_PASSWORD)
+
     expect(myTPLink.token).to.exist
     expect(myTPLink.termid).to.exist
-    // console.log(myTPLink)
+
+    console.log(myTPLink)
+
     const deviceList = await myTPLink.getDeviceList()
-    // console.log(deviceList)
+    console.log(deviceList)
+
     expect(deviceList).that.is.not.empty
-    const power = await myTPLink.getHS110(process.env.TEST_TPLINK_ALIAS).getPowerUsage()
-    // console.log(power)
-    expect(power.power).to.exist
+
+    //var myPlug = myTPLink.getHS100("My Smart Plug");
+    var myPlug = myTPLink.getHS100(process.env.TEST_TPLINK_ALIAS);
+
+    console.log("deviceId=" + myPlug.getDeviceId())
+
+    console.log(myPlug);
+
+    const relay_state = await myPlug.get_relay_state();
+
+    console.log(relay_state)
+
+    //expect(power.power).to.exist
   })
 
 })
